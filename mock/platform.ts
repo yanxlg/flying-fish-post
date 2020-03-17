b9f1cf12dc54665d3b0de03244c45c478d53fa97
xlyan <xlyan@vova.com.hk>
2020-03-17 10:40:24 +0800

    format

--- mock/platform.ts
+++ mock/platform.ts
@@ -1,39 +1,38 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
 
-import Mock, { Random } from 'mockjs';
+import Mock, { Random } from "mockjs";
 
 const list = Mock.mock({
-    'data|100-500': [
+    "data|100-500": [
         {
-            pid: 'pid',
-            platform_id: 'platform_id',
-            platform_name: 'platform_name',
-            app_key: 'app_key',
-            app_secret: 'app_secret',
-            create_time: 'create_time',
-            status: 'status'
-        },
-    ],
+            "pid": "pid",
+            "platform_id": "platform_id",
+            "platform_name": "platform_name",
+            "app_key": "app_key",
+            "app_secret": "app_secret",
+            "create_time": "create_time",
+            "status": "status"
+        }
+    ]
 });
 
-
 export default {
-    'GET /v1/platform/list': (req: Request, res: Response) => {
+    "GET /v1/platform/list": (req: Request, res: Response) => {
         const { page, page_count } = req.query;
         setTimeout(() => {
             res.status(200).send({
                 code: 200,
-                message: 'By mock.js',
+                message: "By mock.js",
                 data: {
                     list: list.data.slice(
                         Number(page_count) * (Number(page) - 1),
-                        Number(page_count) * Number(page),
+                        Number(page_count) * Number(page)
                     ),
                     all_count: list.data.length,
                     page: page,
-                    size: page_count,
-                },
+                    size: page_count
+                }
             });
         }, 500);
-    },
+    }
 };
